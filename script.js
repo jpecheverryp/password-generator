011ba4d6e6efbb57b80243a063e7ba653a728d34
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2020-12-27 18:32:58 -0500

    created function that builds an array if the user wants lowercase characters on the password and console logs it

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var uppercaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
+// Function that asks the user different questions to determine the characters to be used on the password
+function desiredCharacters() {
+  var characters = [];
+  var wantLowercase = confirm('Would you like to have lowercase characters?');
+  if(wantLowercase) {
+    characters = characters.concat(lowercaseAlphabet);
+  }
+
+  console.log(characters);
+}
 
 // Write password to the #password input
 function writePassword() {
-  
+  desiredCharacters();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
