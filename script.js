a5ecf33c82315cea802f4a1c86a0cd6fd10c8ac7
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2020-12-27 20:50:23 -0500

    added symbols to building array function

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 var uppercaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var digitsArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var digitsArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var symbolsArray = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}', '~'];
 
 // Function that asks the user different questions to determine the characters to be used on the password
 function desiredCharacters() {
@@ -20,12 +21,16 @@ function desiredCharacters() {
     characters = characters.concat(digitsArray);
   }
 
-  console.log(characters);
+  if(confirm('Would you like to add symbols?')) {
+    characters = characters.concat(symbolsArray);
+  }
+
+  return characters;
 }
 
 // Write password to the #password input
 function writePassword() {
-  desiredCharacters();
+  console.log(desiredCharacters());
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
