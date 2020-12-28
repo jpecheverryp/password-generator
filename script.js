1768f5475049f6a66bddca2fb5f24748b02debf1
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2020-12-27 20:41:17 -0500

    added numbers to building array function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 var uppercaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+var digitsArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 // Function that asks the user different questions to determine the characters to be used on the password
 function desiredCharacters() {
@@ -13,8 +13,13 @@ function desiredCharacters() {
   }
 
   if(confirm('Would you like to add uppercase characters?')) {
-    characters = characters.concat(uppercaseAlphabet)
+    characters = characters.concat(uppercaseAlphabet);
   }
+
+  if(confirm('Would you like to add numbers?')) {
+    characters = characters.concat(digitsArray);
+  }
+
   console.log(characters);
 }
 
