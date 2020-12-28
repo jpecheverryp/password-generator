9cf7385e275ac4574b79ae0b7767750ead807d2c
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2020-12-27 21:12:14 -0500

    created function that generates a password with a chosen length and array of characters

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ var symbolsArray = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', 
 
 // Function that asks the user different questions to determine the characters to be used on the password
 function desiredCharacters() {
+  // Creates empty array to know desired characters
   var characters = [];
   
+  // Confirms if the user wants every kind of characters and adds them to the array
   if(confirm('Would you like to have lowercase characters?')) {
     characters = characters.concat(lowercaseAlphabet);
   }
@@ -25,13 +27,29 @@ function desiredCharacters() {
     characters = characters.concat(symbolsArray);
   }
 
+  //returns the array with the desired characters
   return characters;
+}
+
+// Function that generates the password takes two parameters the length of the password and an array to be used to generate the password
+function generatePassword(passwordLength, characters) {
+  // Creates an empty string
+  var createdPassword = '';
+  var chosenCharacter = '';
+  console.log(createdPassword);
+  // Iterates with the desired length and adds random characters to the string
+  for(var i = 0; i < passwordLength; i++){
+    // chooses a random character from the array parameter
+    chosenCharacter = characters[Math.floor(Math.random() * characters.length)];
+    // adds the chosen character to the password
+    createdPassword = createdPassword.concat(chosenCharacter);
+  }
+  return createdPassword;
 }
 
 // Write password to the #password input
 function writePassword() {
-  console.log(desiredCharacters());
-  var password = generatePassword();
+  var password = generatePassword(3, desiredCharacters());
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
